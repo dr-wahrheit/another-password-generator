1631c37b92688d2f10e3ec8dfccfb6aed0c7fe22
Stefano <ste.valente@gmail.com>
2025-03-01 11:20:53 +0100

    chore(dev.sh): New "all" command

--- dev.sh
+++ dev.sh
@@ -22,7 +22,8 @@ deploy() {
     echo "Deploying..."
     VERSION=$(get_version)
     cd dist || exit 1
-    git commit -am "v$VERSION"
+    git add .
+    git commit -m "v$VERSION"
     git push
     cd .. || exit 1
 }
@@ -38,24 +39,32 @@ case "$1" in
     update-version)
         update_version
         ;;
+    all)
+        update_version
+        build
+        deploy
+        ;;
     *)
-        echo "Utilizzo: $0 {build|deploy|update-version}
+        echo "Utilizzo: $0 {build|deploy|update-version|all}
 
 Comandi disponibili:
   build                      Compila il progetto utilizzando npm.
   deploy                     Legge la versione dal package.json e la usa per il commit e il push.
   update-version             Incrementa la versione nel package.json.
+  all                        Esegue tutti i comandi in sequenza.
 
 Esempi:
   $0 build                   # Esegue la compilazione del progetto.
   $0 deploy                  # Esegue il deploy con il commit della versione corrente.
   $0 update-version          # Incrementa la versione nel package.json.
+  $0 all                     # Esegue tutti i comandi in sequenza.
 
 Note:
 - Assicurarsi di avere npm installato e le dipendenze configurate prima di eseguire il comando.
 - Il comando 'build' genera la build e imposta la directory .git per la cartella dist.
 - Il comando 'deploy' legge la versione dal package.json e la usa per il commit.
-- Il comando 'update-version' incrementa l'ultima cifra della versione nel package.json."
+- Il comando 'update-version' incrementa l'ultima cifra della versione nel package.json.
+- Il comando 'all' esegue tutti i comandi in sequenza."
         exit 1
         ;;
 esac
